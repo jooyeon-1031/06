c48efb6ca479e3af3d7ab51c00788706b279c1de
jooyeon-1031 <jmhjoin@naver.com>
2020-10-08 15:54:14 +0900

    02

--- main.c
+++ main.c
@@ -3,19 +3,28 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int starPrint()
+int sumTwo(int a, int b)
 {
-	int i;
-	for (i=0; i<10; i++)
-	 	 printf("*");
+  return (a+b);
+}
+	 
+int square(int n)
+{
+	return (n*n);
 }
 
-int main(int argc, char *argv[]){
+int get_max( int x, int y)
+{
+if( x > y) 
+	 return x;
+else 
+	 return y;
+}
 
-	 
-	 starPrint();
-	 starPrint();
-	 starPrint();
-	 
+int main(int argc, char *argv[]) {
+  printf("sumTwo : %i\n" , sumTwo(1,2));
+  printf("square : %i\n" , square(5));
+  printf("get_max : %i\n" ,get_max(10,30));
+	
 	return 0;
 }
